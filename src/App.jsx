3115abed81ad9291022c2cdf44eaa504633dc03c
Deepak-thaur-321 <deepakthakur17756@gmail.com>
2025-10-10 18:12:47 +0530

    Correct folder Structure and imporve Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Product from "../src/components/Product/Product";
+// import Product from "../src/components/Product/Product";
 import Cart from "../src/components/Product/Cart";
-import Navbar from "./components/Product/Navbar";
+import Navbar from "./components/layout/Navbar";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <Router>
-        <Navbar></Navbar>
+      <Navbar></Navbar>
       <Routes>
-        <Route path="/" element={<Product />} />
+        <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/product/:id" element={<Product />} key="product"></Route>
       </Routes>
